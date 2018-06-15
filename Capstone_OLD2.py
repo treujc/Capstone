00e0af8d60c23d6e295d44569e9dac144c2a4e47
treujc <evan.trevathan@bpx.com>
2018-06-15 12:27:25 -0500

    sync

--- Capstone_OLD2.py
+++ Capstone_OLD2.py
@@ -2,9 +2,13 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.model_selection import cross_val_score, cross_val_predict, KFold, train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.linear_model import LinearRegression, Ridge
 import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
+
+
+
 
 #file locations
 current_dir = os.path.realpath(".")
@@ -14,6 +18,8 @@ dataheaderfile = os.path.join(data_folder,'CapstoneDataHeaders.csv')
 #Read in data files.
 df_headers = list(pd.read_csv(dataheaderfile))
 df = pd.read_csv(datafile,names = df_headers, low_memory = False)
+print(df.head())
+
 #Trim data table
 useful_columns = ['BusinessUnit','eventOccurredDate','event']
 df_trimmed = df[useful_columns].reindex(columns = useful_columns)
@@ -57,24 +63,66 @@ def add_rolling_sums(dfBU,DaysToRollList,DaysToPredict):
     dfBU.reset_index()
     
 
-#def rmse(true, predicted):
-#    '''
-#    Calculates Root Mean Square Error.
-#    
-#    Input: true = Observed values, predicted = Results from model prediction.
-#    
-#    Output: RMSE.
-#    '''
-#    diff = (true - predicted)
-#    sumsq = np.sum(diff**2)
-#    count = diff.shape[0]
-#    result = np.sqrt(sumsq/count)
-#    return result
+def rmse(true, predicted):
+    '''
+    Calculates Root Mean Square Error.
+    
+    Input: true = Observed values, predicted = Results from model prediction.
+    
+    Output: RMSE.
+    '''
+    diff = (true - predicted)
+    sumsq = np.sum(diff**2)
+    count = diff.shape[0]
+    result = np.sqrt(sumsq/count)
+    return result
+
+
+def run_model(model,dfBU):
+    '''
+    Runs the model passed into the function.
+    
+    Input:  model = Type of model.
+            ,dfBU = A dataframe trimmed down to one BU.
+    
+    Output: 
+    '''
+    X = pd.DataFrame(dfBU, columns=Predictors)
+    y = np.ravel(pd.DataFrame(dfBU, columns=To_Predict))
+    #Do initial Test-Train split.
+    X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = .2,random_state = seed)
+    results = cross_val_score(model, X_train, y_train, cv=kfold)
+    predicted = cross_val_predict(model, X_train, y_train, cv=kfold)
+    r2 = results.mean()
+#    return print(r2)
+    #Scatter plot of Predicted vs Measured.
+    plt.figure(figsize= (figure_size,figure_size))
+    ax = plt.subplot()
+    ax.scatter(y_train, predicted, edgecolors=(.5,1,0))
+    ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
+    plt.suptitle('Measured vs Predicted for {} BU'.format(BU),fontsize = 12)
+    plt.title('r2: {:.2f}'.format(r2),fontsize = 12)
+    ax.set_xlabel('Measured')
+    ax.set_ylabel('Predicted')
+    #print('{} BU, r2: {:.2f}'.format(BU,r2))
+    plt.show()
+    #Plot over time where actuals are a scatter plot and prediction is the line.
+    #plt.figure(figsize= (figure_size,figure_size))
+    #plt.plot(predicted)
+    #Plot Feature Importances
+    model.fit(X_train,y_train)
+    importances = model.feature_importances_
+    indices = np.argsort(importances)
+    plt.title('Feature Importances')
+    plt.barh(range(len(indices)), importances[indices], color='b', align='center')
+    plt.yticks(range(len(indices)), Predictors)
+    plt.xlabel('Relative Importance')
+    plt.show()
 
 
 
 if __name__ == '__main__':  
-    DaysToRollList = [3,7,14,30,45]
+    DaysToRollList = [3,7,15,30,45]
     DaysToPredict = 14
     figure_size = 5
     ###Identify Predictors and what I am predicting.
@@ -82,12 +130,12 @@ if __name__ == '__main__':
                     'WeekDay',
                     'event_Incident_Rolling3',
                     'event_Incident_Rolling7',
-                    'event_Incident_Rolling14',
+                    'event_Incident_Rolling15',
                     'event_Incident_Rolling30',
                     'event_Incident_Rolling45',
                     'event_Observation_Rolling3',
                     'event_Observation_Rolling7',
-                    'event_Observation_Rolling14',
+                    'event_Observation_Rolling15',
                     'event_Observation_Rolling30',
                     'event_Observation_Rolling45'
                     ]
@@ -95,7 +143,6 @@ if __name__ == '__main__':
     seed = 9
     kfold = KFold(n_splits=3, random_state=seed, shuffle = True)
     for BU in list(dfFinal.BusinessUnit.unique()):  #['Midcon']:
-        print('-------------------------------{} BU-----------------------------------'.format(BU))
         dfBU = pd.DataFrame(dfFinal[(dfFinal['BusinessUnit'] == BU)].copy())
         add_rolling_sums(dfBU,DaysToRollList,DaysToPredict) 
         MinDate = '1/1/2018'
@@ -105,43 +152,19 @@ if __name__ == '__main__':
 #        scatter_matrix(dfBU, alpha=0.4, figsize=(figure_size,figure_size), diagonal='kde')
 #        plt.suptitle('Scatter Matrix for {} BU'.format(BU),fontsize = 12)
 #        plt.show()
-        X = pd.DataFrame(dfBU, columns=Predictors)
-        y = np.ravel(pd.DataFrame(dfBU, columns=To_Predict))
+#        print('-------------------------Linear Regression---------------------------')
+#        model = LinearRegression()
+#        run_model(model,dfBU)
+#        print('---------------------------------------------------------------------')
+#        print('-----------------------Random Forest Regressor-----------------------')
         model = RandomForestRegressor(n_estimators = 50)#,bootstrap = True)
-        #Do initial Test-Train split.
-        X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = .2,random_state = seed)
-        results = cross_val_score(model, X_train, y_train, cv=kfold)
-        predicted = cross_val_predict(model, X_train, y_train, cv=kfold)
-        r2 = results.mean()
-        #Scatter plot of Predicted vs Measured.
-        plt.figure(figsize= (figure_size,figure_size))
-        ax = plt.subplot()
-        ax.scatter(y_train, predicted, edgecolors=(.5,1,0))
-        ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-        plt.suptitle('Measured vs Predicted, r2: {:.2f}'.format(r2),fontsize = 12)
-        ax.set_xlabel('Measured')
-        ax.set_ylabel('Predicted')
-        plt.show()
-        #Plot Feature Importances
-        model.fit(X_train,y_train)
-        importances = model.feature_importances_
-        indices = np.argsort(importances)
-        plt.title('Feature Importances')
-        plt.barh(range(len(indices)), importances[indices], color='r', align='center')
-        plt.yticks(range(len(indices)), Predictors)
-        plt.xlabel('Relative Importance')
-        plt.show()
-        # The baseline predictions are the historical averages
-        baseline_preds = X_test['event_Incident_Rolling14']
-        # Baseline errors, and display average baseline error
-        baseline_errors = abs(baseline_preds - y_test)
-        print('Average baseline error: ', round(np.mean(baseline_errors), 2))
-        # Use the forest's predict method on the test data
-        predictions = model.predict(X_test)
-        # Calculate the absolute errors
-        errors = abs(predictions - y_test)
-        # Print out the mean absolute error (mae)
-        print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+        run_model(model,dfBU)
+#        print('---------------------------------------------------------------------')
+#        print('---------------------------Ridge Regression--------------------------')
+#        model = Ridge(alpha = .5,normalize = True)
+#        run_model(model,dfBU)
+#        print('---------------------------------------------------------------------')
+
 
 #Are there different types of Observations? Maybe bucket Spills differently than cuts etc. 
 #Consider Poisson Regression. #Seems very difficult to implement. Add this as a how to possibly improve note.
